bdcbb4c63aa2dca8eca61c8ba2f353d88cccb521
Sam Marks <sam@sammarks.me>
2023-01-22 11:25:07 -0500

    Initial Commit

---
+++ DataBar/DataBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataBarApp.swift
+//  DataBar
+//
+//  Created by Sam Marks on 1/22/23.
+//
+
+import SwiftUI
+
+@main
+struct DataBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
